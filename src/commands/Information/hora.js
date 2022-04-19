429b9b1c4383592d1a2ace5c702017745ed1be9a
anzaldoivan <anzaldoivan98@gmail.com>
2022-04-18 21:17:37 -0300

    date test

---
+++ src/commands/Information/hora.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const Discord = require("discord.js");
+const { DateTime } = require("luxon");
+const decache = require("decache");
+const funcDate = require("../../utils/getDate.js");
+const funcMatches = require("../../utils/getMatches.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("nashe").setDescription("NASHE"),
+  async execute(interaction, client) {
+    const hora = DateTime.local;
+    const horaARG = hora.setZone("Argentina/Buenos_Aires");
+
+    await interaction.followUp(`Hora del SV: ${hora}\nHora local: ${horaARG}`);
+  },
+};

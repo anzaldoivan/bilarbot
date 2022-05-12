9461930f0aa717cfce00658523b1ea49707c5acc
anzaldoivan <anzaldoivan98@gmail.com>
2022-05-12 10:52:27 -0300

    unsigneame al br

--- src/commands/ELO/list.js
+++ src/commands/ELO/list.js
@@ -9,6 +9,9 @@ module.exports = {
     .setDescription("Ver lista de espera de Matchmaking ELO."),
   channel: ["779460129065009172", "898701693741596692"],
   async execute(interaction, client) {
+    const embed = new MessageEmbed().setImage(
+      "https://media.discordapp.net/attachments/455510792469479446/754851516971745362/brr.jpg"
+    );
     const signedList = await package.signedList(client.config, interaction);
     const row = new MessageActionRow().addComponents(
       new MessageButton()
@@ -47,10 +50,7 @@ module.exports = {
         .setStyle("DANGER")
     );
 
-    await interaction.editReply(
-      "https://media.discordapp.net/attachments/455510792469479446/754851516971745362/brr.jpg",
-      { components: [row2] }
-    );
+    await interaction.editReply({ embeds: [embed], components: [row2] });
     await interaction.followUp({ embeds: [signedList], components: [row] });
   },
 };
